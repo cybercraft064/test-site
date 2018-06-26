70f02f230d4633c8b70e2e2d89217143eded03d4
Pizza064 <cybercraft64@hotmail.fr>
2018-06-26 23:32:38 +0200

    page badge

--- src/badge/badges.php
+++ src/badge/badges.php
@@ -15,7 +15,7 @@ include('./logic/badges-logic.php');
         <title>Badges</title>
     </head> 
     <body>
-        <section class="page-container ">
+        <section class="page-container">
                 
             <div class="page-filter">
 
@@ -29,26 +29,96 @@ include('./logic/badges-logic.php');
                             <div class="barre"></div>
                         </div>
                         <ul class="burger-menu">
-                        <i class="burger-icon-level icon-level"></i>
-                        <li><a href="../dashboard/dashboard-levels.php?level=<?= $_SESSION['validated-level-bd']; ?>">Niveaux</a></li>
-                        <i class="burger-icon-langue icon-langue"></i>
-                        <li><a href="../language/choose-langues.php">Choix des Langues</a></li>
-                        <div class="borderLine"></div> 
-                        <i class="burger-icon-user icon-user"></i>
-                        <li><a href="../auth/start-account.php">Ton compte</a></li>
-                        <i class="burger-icon-exit icon-exit"></i>
-                        <li><a href="../auth/logout.php">déconnexion</a></li>
+                            <i class="burger-icon-level icon-level"></i>
+                            <li> <a href="../dashboard/dashboard-levels.php?level=<?= $_SESSION['validated-level-bd']; ?>"> Niveaux </a></li>
+                            <i class="burger-icon-langue icon-langue"></i>
+                            <li> <a href="../language/choose-langues.php"> Choix des Langues </a> </li>
+                            <div class="borderLine"></div> 
+                            <i class="burger-icon-user icon-user"></i>
+                            <li> <a href="../auth/start-account.php"> Ton compte </a> </li>
+                            <i class="burger-icon-exit icon-exit"></i>
+                            <li> <a href="../auth/logout.php"> déconnexion </a> </li>
                         </ul>
                     </div> 
                 </header>
 
-                <section class="title Foswald ">
-                    <span class="line-txt"></span>
-                    <span class="fat-300"> Tes <span class="fat-400">badges</span>
-                    <span class="line-txt"></span>
+                <section class="title Foswald">
+                    <span class="line-txt"> </span>
+                    <span class="fat-300"> Tes <span class="fat-400"> Badges </span>
+                    <span class="line-txt"> </span>
                 </section>
 
                 <section class="middle">
+
+                    <section class="current-badge">
+                        <img src="../../assets/img/badges/<?= $currentBadge; ?>" alt="badge en cours" class="img-current-badge" />
+                    </section>
+
+                    <section class="next-badge-container">
+                        <div class="title-badge Foswald fat-300">
+                            <?= $titleBadge; ?> <br>
+                            <span class="km-next-badge fat-400"> <?= $currentKm; ?> km </span>
+                        </div>
+                        <div class="progress-bar-container">
+                            <div class="next-badge">
+                                <img src="../../assets/img/badges/<?= $nextBadge; ?>" alt="badge suivant" class="img-next-badge" />    
+                            </div>
+                            <div class="progress-bar">
+                                <div class="run-Progress-bar"> </div>
+                            </div>
+                            <div class="txt-progress-bar "> Plus que <?= $kmNextBadge; ?> km avant le prochain badge </div>      
+                        </div>
+                    </section>
+
+                    <section class="challenge-badge-container">
+                        <div class="challenge-badge">
+                            <img src="../../assets/img/badges/<?= $challengeBadge; ?>" class="img-challenge-badge" />         
+                            <div class="title-challenge-badge Foswald fat-300">
+                            <?= $titleChallengeBadge; ?> <br>
+                            <span class="km-challenge-badge fat-400"> <?= $KmChallenge; ?> km </span>
+                            </div>
+                        </div> 
+                        <div class="challenge-badge">
+                        <img src="../../assets/img/badges/<?= $challengeBadge; ?>" class="img-challenge-badge" />         
+                            <div class="title-challenge-badge Foswald fat-300">
+                            <?= $titleChallengeBadge; ?> <br>
+                            <span class="km-challenge-badge fat-400"> <?= $KmChallenge; ?> km </span>
+                            </div>
+                        </div>
+                        <div class="challenge-badge">
+                        <img src="../../assets/img/badges/<?= $challengeBadge; ?>" class="img-challenge-badge" />         
+                            <div class="title-challenge-badge Foswald fat-300">
+                            <?= $titleChallengeBadge; ?> <br>
+                            <span class="km-challenge-badge fat-400"> <?= $KmChallenge; ?> km </span>
+                            </div>
+                        </div>
+                        <div class="challenge-badge">
+                        <img src="../../assets/img/badges/<?= $challengeBadge; ?>" class="img-challenge-badge" />         
+                            <div class="title-challenge-badge Foswald fat-300">
+                            <?= $titleChallengeBadge; ?> <br>
+                            <span class="km-challenge-badge fat-400"> <?= $KmChallenge; ?> km </span>
+                            </div>
+                        </div>
+                        <div class="challenge-badge">
+                        <img src="../../assets/img/badges/<?= $challengeBadge; ?>" class="img-challenge-badge" />         
+                            <div class="title-challenge-badge Foswald fat-300">
+                            <?= $titleChallengeBadge; ?> <br>
+                            <span class="km-challenge-badge fat-400"> <?= $KmChallenge; ?> km </span>
+                            </div>
+                        </div>
+                        <div class="challenge-badge">
+                        <img src="../../assets/img/badges/<?= $challengeBadge; ?>" class="img-challenge-badge" />         
+                            <div class="title-challenge-badge Foswald fat-300">
+                            <?= $titleChallengeBadge; ?> <br>
+                            <span class="km-challenge-badge fat-400"> <?= $KmChallenge; ?> km </span>
+                            </div>
+                        </div>  
+
+                    </section>
+
+                    <section class="general-progress-bar">
+
+                    </section>
 
                 </section>
 
--- src/badge/logic/badges-logic.php
+++ src/badge/logic/badges-logic.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
 
-$currentCodeLang = htmlspecialchars($_SESSION['current-code-language']);
+  $currentCodeLang = htmlspecialchars($_SESSION['current-code-language']);
+
+// Variables de contexte
+$currentBadge = "appareil-avec-photos.png";
+$titleBadge = "voyageur junior";
+$currentKm = "118";
+$nextBadge = "faceBook.png";
+$kmNextBadge = "6";
+$challengeBadge = "faceBook.png";
+$titleChallengeBadge = "Roi des touristes";
+$KmChallenge = "50";
+
 
